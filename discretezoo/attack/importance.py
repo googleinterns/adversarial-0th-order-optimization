52bafc0ba47d102f9c7fe9c99a42aaf76dbd0ad1
bergern <bergern@google.com>
2020-08-26 11:20:21 +0000

    Added importance scoring and token dropping helper functions.
    In order to decide which tokens to change we need to know which
    tokens affect the output of our model the most. The scorer
    function works by iterating over each token in the sentences and
    either masking them or deleting them and then computing the
    difference in model output, given by a forward function.
    
    Change-Id: Ie394411fbcdcca720097af298cf9c530e3151564

---
+++ discretezoo/attack/importance.py
@@ -0,0 +1,81 @@
+"""Contains the importance scorer for deciding which token to change."""
+import tensorflow as tf
+from typing import Callable, Dict, Any
+
+
+def mask_position(sentences: tf.Tensor, position: int, **kwargs) -> tf.Tensor:
+  """Masks a single position in all sentences with a given integer id.
+
+  Arguments:
+    sentences: This is a batch of sentences <int32>[batch_size, length].
+    position: Position is the index of the token in sentences to be masked.
+    kwargs: A dict that must contain a value for the key 'mask_id'.
+
+  Returns:
+    A batch of sentences with the token at position replaced with mask_id.
+  """
+  assert 'mask_id' in kwargs
+  assert position >= 0
+  assert position < sentences.shape[1]
+  mask_vector = tf.reshape(
+      tf.constant([[kwargs['mask_id']] * sentences.shape[0]]), (-1, 1))
+  masked_sentences = tf.concat(
+      [sentences[:, :position], mask_vector, sentences[:, position + 1:]], 1)
+  return masked_sentences
+
+
+def drop_position(sentences: tf.Tensor, position: int, **kwargs) -> tf.Tensor:
+  """Drops a single token at position from every sentence in sentences.
+
+  Arguments:
+    sentences: This is a batch of sentences <int32>[batch_size, length].
+    position: Position is the index of the token in sentences to be dropped.
+
+  Returns:
+    A batch of sentences with the token at position removed.
+  """
+  assert position >= 0
+  assert position < sentences.shape[1]
+  dropped_sentences = tf.concat(
+      [sentences[:, :position], sentences[:, position + 1:]], 1)
+  return dropped_sentences
+
+
+def scorer(sentences: tf.Tensor,
+           output_difference_fn: Callable[[tf.Tensor, tf.Tensor], tf.Tensor],
+           removal_fn: Callable[[tf.Tensor, int], tf.Tensor],
+           removal_fn_kwargs: Dict[str, Any] = {},
+           pad_id: int = 0) -> tf.Tensor:
+  """Performs importance scoring for each token in the input sentences.
+
+  This function loops over every token in the input sentences and either drops
+  it or masks it.
+
+  Args:
+    sentences: A tensor <int32>[batch_size, sentence_length] of token ids.
+      Sentences of different lengths are assumed to be padded with pad_id.
+    output_difference_fn: A callable that returns the output difference between
+      inputs. First input is the sentences with the dropped/masked token.
+      Second input is the original sentences. Outputs are in [0, +inf]. Must
+      return a <float32>[batch_size, 1] tensor.
+    removal_fn: A callable that modifies a single position in the input
+      sentences so that the importance of that position can be evaluated.
+    removal_fn_kwargs: A dictionary of keyword arguments for removal_fn.
+    pad_id: The index of the padding token in the vocabulary. This is used so
+      padded positions can't receive non-zero importance scores.
+
+  Returns:
+    A matrix containing the token importance scores for each sentence.
+      <float32>[batch_size, sentence_length]
+  """
+  position_mask = tf.where(sentences == pad_id, 0.0, 1.0)
+  differences = []
+
+  for position in range(sentences.shape[-1]):
+    dropped_token_sentences = removal_fn(sentences, position,
+                                         **removal_fn_kwargs)
+    loss_differences = output_difference_fn(dropped_token_sentences, sentences)
+    differences.append(loss_differences)
+
+  difference_matrix = tf.concat(differences, -1)
+  return position_mask * difference_matrix
---
+++ discretezoo/attack/importance_test.py
@@ -0,0 +1,136 @@
+"""Contains the importance scorer tests."""
+import tensorflow as tf
+
+from discretezoo.attack import importance
+
+from absl import flags
+from absl.testing import absltest
+
+
+class ScorerTest(absltest.TestCase):
+  """This class contains the tests for the functions inside of importance.py."""
+
+  def test_scorer(self):
+    """This tests the importance scorer as a whole."""
+
+    def output_difference_fn(deleted_token_sentence, original_sentences):
+      return tf.cast(
+          tf.math.reduce_sum(deleted_token_sentence, axis=-1, keepdims=True),
+          tf.float32)
+
+    def dropper_function(sentences, position):
+      return sentences
+
+    sentence = tf.expand_dims(tf.range(0, 10), axis=0)
+    expected_scores = tf.constant([[45.0] * 10])
+    test_scores = importance.scorer(sentence,
+                                    output_difference_fn,
+                                    dropper_function,
+                                    pad_id=-1)
+    tf.debugging.assert_equal(expected_scores, test_scores)
+
+  def test_ignore_padding(self):
+    """This tests whether or not padding tokens are ignored in scoring."""
+
+    def output_difference_fn(deleted_token_sentence, original_sentences):
+      return tf.cast(
+          tf.math.reduce_sum(deleted_token_sentence, axis=-1, keepdims=True),
+          tf.float32)
+
+    def dropper_function(sentences, position):
+      return sentences
+
+    sentences = tf.constant([[1, 1, 1, 1, 1], [1, 1, 1, 1, 0], [1, 1, 1, 0, 0],
+                             [1, 1, 0, 0, 0], [1, 0, 0, 0, 0]])
+    expected_scores = tf.constant([[5, 5, 5, 5, 5], [4, 4, 4, 4, 0],
+                                   [3, 3, 3, 0, 0], [2, 2, 0, 0, 0],
+                                   [1, 0, 0, 0, 0]])
+    test_scores = importance.scorer(sentences, output_difference_fn,
+                                    dropper_function)
+    tf.debugging.assert_equal(expected_scores, test_scores)
+
+
+class MaskPositionTest(absltest.TestCase):
+
+  def test_mask_position(self):
+    """This tests _mask_position to see if it correctly masks target tokens."""
+    sentence = tf.expand_dims(tf.range(1, 11), 0)
+    expected_masked_position = tf.constant([[1, 2, 3, 4, 5, 0, 7, 8, 9, 10]],
+                                           dtype=tf.int32)
+    position = 5
+    test_masked_position = importance.mask_position(sentence,
+                                                    position,
+                                                    mask_id=0)
+    tf.debugging.assert_equal(expected_masked_position, test_masked_position)
+
+  def test_masking_last_position(self):
+    """This test is for the edge case where we mask the last token."""
+    sentence = tf.expand_dims(tf.range(1, 11), 0)
+    expected_last_token_masked_sentence = tf.constant(
+        [[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]])
+    position = 9
+    test_last_token_masked_sentence = importance.mask_position(sentence,
+                                                               position,
+                                                               mask_id=0)
+    tf.debugging.assert_equal(expected_last_token_masked_sentence,
+                              test_last_token_masked_sentence)
+
+  def test_mask_position_too_big_index(self):
+    """This test is to ensure out of bounds (too big) positions get caught."""
+    sentence = tf.expand_dims(tf.range(1, 11), 0)
+    position = 11
+    with self.assertRaises(AssertionError):
+      importance.mask_position(sentence, position, mask_id=0)
+
+  def test_mask_position_negative_index(self):
+    """This test ensures negative positions (while valid python) aren't used."""
+    sentence = tf.expand_dims(tf.range(1, 11), 0)
+    position = -1
+    with self.assertRaises(AssertionError):
+      importance.mask_position(sentence, position, mask_id=0)
+
+  def test_mask_position_mask_id_requirement(self):
+    """This test makes sure mask_position checks for the mask_id argument."""
+    sentence = tf.expand_dims(tf.range(1, 11), 0)
+    position = 5
+    with self.assertRaises(AssertionError):
+      importance.mask_position(sentence, position)
+
+
+class DropPositionTest(absltest.TestCase):
+
+  def test_drop_position(self):
+    """This tests _mask_position to see if it correctly drops target tokens."""
+    sentence = tf.expand_dims(tf.range(0, 10), 0)
+    expected_dropped_position = tf.constant([[0, 1, 2, 3, 5, 6, 7, 8, 9]],
+                                            dtype=tf.int32)
+    position = 4
+    test_dropped_position = importance.drop_position(sentence, position)
+    tf.debugging.assert_equal(expected_dropped_position, test_dropped_position)
+
+  def test_dropping_last_position(self):
+    """This test is for the edge case where we drop the last token."""
+    sentence = tf.expand_dims(tf.range(0, 10), 0)
+    expected_last_token_dropped_sentence = tf.expand_dims(tf.range(0, 9), 0)
+    position = 9
+    test_last_token_dropped_sentence = importance.drop_position(sentence, 9)
+    tf.debugging.assert_equal(expected_last_token_dropped_sentence,
+                              test_last_token_dropped_sentence)
+
+  def test_drop_position_too_big_index(self):
+    """This test is to ensure out of bounds (too big) positions get caught."""
+    sentence = tf.expand_dims(tf.range(1, 11), 0)
+    position = 11
+    with self.assertRaises(AssertionError):
+      importance.drop_position(sentence, position)
+
+  def test_drop_position_negative_index(self):
+    """This test ensures negative positions (while valid python) aren't used."""
+    sentence = tf.expand_dims(tf.range(1, 11), 0)
+    position = -1
+    with self.assertRaises(AssertionError):
+      importance.drop_position(sentence, position)
+
+
+if __name__ == '__main__':
+  absltest.main()
