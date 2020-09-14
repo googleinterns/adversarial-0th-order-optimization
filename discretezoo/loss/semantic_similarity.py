822984640e09f480fcfd759cad70fe16ef45cb21
bergern <bergern@google.com>
2020-09-14 09:49:51 +0000

    Adds classes for calculating distances from embedded sentences.
    This commit adds two classes, EmbeddedCosineDistance and
    EmbeddedEuclideanDistance. The two classes both accept embeddings
    and are callable. When called the embed the two sentences using
    embeddings, reduces them, and then computes distance.
    
    Change-Id: If8efe534cb84f39ce82e6d7a50166139d02aec70

---
+++ discretezoo/loss/semantic_similarity.py
@@ -0,0 +1,122 @@
+import tensorflow as tf
+import tensorflow_hub as tfhub
+
+from typing import Callable
+
+
+class EmbeddedCosineDistance:
+  """EmbeddedCosineDistance calculates cosine distance in embedding space.
+
+  Attributes:
+    embeddings: A tensor containing an embedding vector for each index in vocab.
+      <float32>[vocab_size, embedding_dimension]
+  """
+
+  def __init__(self, embeddings: tf.Tensor):
+    """Initializes EmbeddedCosineDistance with embeddings.
+
+    Arguments:
+      embeddings: A tensor containing an embedding for each index in vocab.
+        <float32>[vocab_size, embedding_dimension]
+    """
+    assert embeddings.ndim == 2, ('Embeddings are expected to have 2 dimensions'
+      f' but you passed a tensor with {embeddings.ndim}.')
+    self._embeddings = embeddings
+
+  def __call__(self, original_sentences: tf.Tensor,
+               adversarial_sentences: tf.Tensor) -> tf.Tensor:
+    r"""Calculates cosine distance between reduced embedded sentences.
+
+    Sentences are embedded and then reduced by summing them up.
+    Cosine similarity is then given by \frac{v_{original} \cdot v_{adversarial}}
+      {|v_{original}| \times |v_{adversarial|}}.
+    Cosine distance is defined as 1 - similarity.
+
+    Arguments:
+      original_sentences: A tensor of token indices in the original sentences.
+        <int32>[batch_size, sentence_length]
+      adversarial_sentences: A tensor of token indices in the adversarial
+        sentences. <int32>[batch_size, sentence_length]
+
+    Returns:
+      A tensor <float32>[batch_size, 1] of cosine distances between original and
+        adversarial sentences. Return values are in the range [0, 2]
+        https://www.tensorflow.org/api_docs/python/tf/keras/losses/cosine_similarity
+        is used internally, which computes negative similarity, and 1 is added.
+    """
+    original_sentences_embedded = tf.nn.embedding_lookup(
+        self._embeddings, original_sentences)
+    adversarial_sentences_embedded = tf.nn.embedding_lookup(
+        self._embeddings, adversarial_sentences)
+
+    original_sentences_reduced = tf.math.reduce_sum(original_sentences_embedded,
+                                                    axis=1)
+    adversarial_sentences_reduced = tf.math.reduce_sum(
+        adversarial_sentences_embedded, axis=1)
+
+    # Unintuitively, tf.keras.losses.cosine_similarity returns distances in
+    # [-1, 1]. Adding 1 means that two vectors will have 0 as a minimum distance
+    # instead of -1, which is helpful in later loss computation.
+    distance = 1 + tf.keras.losses.cosine_similarity(
+        original_sentences_reduced, adversarial_sentences_reduced)
+    return distance
+
+
+class EmbeddedEuclideanDistance:
+  """EmbeddedEuclideanDistance calculates euclidean distance in embedding space.
+
+  Attributes:
+    embeddings: A tensor containing an embedding vector for each index in vocab.
+      <float32>[vocab_size, embedding_dimension]
+    reduce_mean: This is a boolean flag that signals how embedded sentences will
+      be reduced to a single vector. True for mean, False for sum.
+  """
+
+  def __init__(self, embeddings: tf.Tensor, reduce_mean: bool = True):
+    """Initializes EmbeddedEuclideanDistance with embeddings and reduction type.
+
+    Arguments:
+      embeddings: A tensor containing an embedding for each index in vocab.
+        <float32>[vocab_size, embedding_dimension]
+      reduce_mean: This boolean flag signals how embedded sentences will be
+        reduced to a single vector. True for mean, False for sum.
+    """
+    assert embeddings.ndim == 2, ('Embeddings are expected to have 2 dimensions'
+      f' but you passed a tensor with {embeddings.ndim}.')
+    self._embeddings = embeddings
+    self._reduce_mean = reduce_mean
+
+  def __call__(self, original_sentences: tf.Tensor,
+               adversarial_sentences: tf.Tensor) -> tf.Tensor:
+    """Calculates euclidean distances between reduced embedded sentences.
+
+    Arguments:
+      original_sentences: A tensor of token indices in the original sentences.
+        <int32>[batch_size, sentence_length]
+      adversarial_sentences: A tensor of token indices in the adversarial
+        sentences. <int32>[batch_size, sentence_length]
+
+    Returns:
+      A tensor <float32>[batch_size, 1] of euclidean distances between original
+        and adversarial sentences.
+    """
+    original_sentences_embedded = tf.nn.embedding_lookup(
+        self._embeddings, original_sentences)
+    adversarial_sentences_embedded = tf.nn.embedding_lookup(
+        self._embeddings, adversarial_sentences)
+
+    if self._reduce_mean:
+      original_sentences_reduced = tf.math.reduce_mean(
+          original_sentences_embedded, axis=1)
+      adversarial_sentences_reduced = tf.math.reduce_mean(
+          adversarial_sentences_embedded, axis=1)
+    else:
+      original_sentences_reduced = tf.math.reduce_sum(
+          original_sentences_embedded, axis=1)
+      adversarial_sentences_reduced = tf.math.reduce_sum(
+          adversarial_sentences_embedded, axis=1)
+
+    difference_vector = tf.math.subtract(original_sentences_reduced,
+                                         adversarial_sentences_reduced)
+    distance = tf.norm(difference_vector, axis=-1, keepdims=True)
+    return distance
---
+++ discretezoo/loss/semantic_similarity_test.py
@@ -0,0 +1,71 @@
+"""Tests for the file semantic_similarity.py"""
+import tensorflow as tf
+
+from absl import flags
+from absl.testing import absltest
+
+from discretezoo.loss import semantic_similarity
+
+
+class SemanticSimilarityTest(absltest.TestCase):
+
+  def test_cosine_distance_mean_reduction(self):
+    """Tests consine similarity between two sentences."""
+    # Points on the unit circle for 0, pi/6, pi/4, pi/3, and pi/2 radians.
+    embeddings = tf.constant([[1., 0.], [0.866025, 0.5], [0.707107, 0.707107],
+                              [0.5, 0.866025], [0., 1.]])
+    # These are the points at 1.0 on the x and y axes, respectively.
+    adversarial_sentences = tf.constant([[0, 4]], dtype=tf.int32)
+    # This is the point at pi/4 radians on the unit circle, twice.
+    original_sentences = tf.constant([[2, 2]], dtype=tf.int32)
+    # The two points at 1.0 on the x and y axes added together and normalized is
+    # the same point as pi/4 on the unit circle, so cosine similarity is 1.
+    # The cosine distance object gives 1 - similarity, so we get 0 as a result.
+    expected_cosine_distance = tf.constant([[0.0]])
+    cosine_distance_object = semantic_similarity.EmbeddedCosineDistance(
+        embeddings)
+    test_cosine_distance = cosine_distance_object(original_sentences,
+                                                  adversarial_sentences)
+    tf.debugging.assert_near(expected_cosine_distance, test_cosine_distance)
+
+  def test_cosine_distance_two_dimensional_embeddings_requirement(self):
+    embeddings = tf.range(0, 10, dtype=tf.float32)
+    with self.assertRaises(AssertionError):
+      cosine_distance_object = semantic_similarity.EmbeddedCosineDistance(
+          embeddings)
+
+  def test_euclidean_distance_sum_reduction(self):
+    """Tests euclidean distance between two sentences."""
+    embeddings = tf.expand_dims(tf.range(0, 10, dtype=tf.float32), -1)
+
+    adversarial_sentences = tf.constant([[0, 8]])
+    original_sentences = tf.constant([[4, 4]])
+    expected_distance = tf.constant([[0.]])
+
+    distance_object = semantic_similarity.EmbeddedEuclideanDistance(
+        embeddings, reduce_mean=False)
+    test_distance = distance_object(original_sentences, adversarial_sentences)
+    tf.debugging.assert_near(expected_distance, test_distance)
+
+  def test_euclidean_distance_mean_reduction(self):
+    """Tests euclidean distance between two sentences."""
+    embeddings = tf.expand_dims(tf.range(0, 10, dtype=tf.float32), -1)
+
+    adversarial_sentences = tf.constant([[0, 7]])
+    original_sentences = tf.constant([[3, 3]])
+    expected_distance = tf.constant([[0.5]])
+
+    distance_object = semantic_similarity.EmbeddedEuclideanDistance(
+        embeddings, reduce_mean=True)
+    test_distance = distance_object(original_sentences, adversarial_sentences)
+    tf.debugging.assert_near(expected_distance, test_distance)
+
+  def test_euclidean_distance_two_dimensional_embeddings_requirement(self):
+    embeddings = tf.range(0, 10, dtype=tf.float32)
+    with self.assertRaises(AssertionError):
+      distance_object = semantic_similarity.EmbeddedEuclideanDistance(
+          embeddings)
+
+
+if __name__ == '__main__':
+  absltest.main()
